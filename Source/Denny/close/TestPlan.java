edb80b7918fe1015107c5135d1aa1b277326c79e
denny <ichlavio@126.com>
2017-07-29 00:11:45 +0900

    TBD

---
+++ Source/Denny/close/TestPlan.java
@@ -0,0 +1,16 @@
+package close;
+
+public class TestPlan {
+	static void addTestCase() {
+		//TBD
+	}
+	static void removeTestCase() {
+		//TBD
+	}
+	static void execute() {
+		//TBD
+	}
+	
+	
+	
+}
